b03bf5f9070f9bd8380001eea3967be408c23ae1
lucifer-430 <54584266+lucifer-430@users.noreply.github.com>
2024-04-20 14:12:26 +0530

    Update questions.sql
    hey! I solved these queries,
    kindly let me know my score.

--- SQL Deep Dive/Comparison Operators/questions.sql
+++ SQL Deep Dive/Comparison Operators/questions.sql
@@ -1,21 +1,20 @@
 -- How many female customers do we have from the state of Oregon (OR)?
-/*
-* Write your query here
-*/
+  
+SELECT count(firstname )from "public"."customers"
+where state='OR' AND gender='F'
 
 -- Who over the age of 44 has an income of 100 000 or more? (excluding 44)
-/*
-* Write your query here
-*/
+  SELECT * from "public"."customers"
+  where age>44 and income>='100000'
 
 -- Who between the ages of 30 and 50 has an income less than 50 000?
 -- (include 30 and 50 in the results)
+  SELECT firstname,age,income from "public"."customers"
+  where (age BETWEEN 30 and 50) and (income<50000)
 
-/*
-* Write your query here
-*/
+
 
 -- What is the average income between the ages of 20 and 50? (Excluding 20 and 50)
-/*
-* Write your query here
-*/
+  
+SELECT avg(income) from "public"."customers"
+where age>20 and age<50
