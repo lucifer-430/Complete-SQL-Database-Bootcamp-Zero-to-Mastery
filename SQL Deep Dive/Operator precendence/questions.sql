2a3b80e0308c1763e3a102df5aeab981a94b9fc5
lucifer-430 <54584266+lucifer-430@users.noreply.github.com>
2024-04-21 00:29:12 +0530

    Update questions.sql
    new one! kindly view this

--- SQL Deep Dive/Operator precendence/questions.sql
+++ SQL Deep Dive/Operator precendence/questions.sql
@@ -7,10 +7,13 @@
 * Include people that are 50
 * that are from either Japan or Australia
 */
-
+SELECT count(firstname) from "public"."customers"
+where (age<30 or age>=50) and (country='Japan' or country='Australia') and income>50000
 /*
 * DB: Store
 * Table: Orders
 * Question: 
 * What was our total sales in June of 2004 for orders over 100 dollars?
 */
+SELect sum(totalamount) from "public"."orders"
+where (totalamount>100 and orderdate >='2004-06-01') or (totalamount>100 and orderdate<='2004-06-30')
