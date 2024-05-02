af56ce402a3f0a737c2ea9e7c34c675ec57a65ff
lucifer-430 <54584266+lucifer-430@users.noreply.github.com>
2024-05-02 17:47:36 +0530

    Update questions.sql

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -3,7 +3,8 @@
 * Table: employees
 * Question: Sort employees by first name ascending and last name descending
 */
-
+SELECT * FROM "public"."employees"
+order by first_name asc,last_name DESC
 
 /*
 * DB: Employees
@@ -11,9 +12,13 @@
 * Question: Sort employees by age
 */
 
-
+SELECT * FROM "public"."employees"
+order by birth_date
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+SELECT * FROM "public"."employees"
+where first_name like 'K%'
+order by hire_date
