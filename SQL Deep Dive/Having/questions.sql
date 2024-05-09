abc637f8206608f8cdfc605940538382b2371fc2
lucifer-430 <54584266+lucifer-430@users.noreply.github.com>
2024-05-10 00:21:18 +0530

    Update questions.sql
    all 3 are correct

--- SQL Deep Dive/Having/questions.sql
+++ SQL Deep Dive/Having/questions.sql
@@ -4,13 +4,26 @@
 *  Database: Employees
 */
 
-
+SELECT a.emp_no,count(b.title) from "public"."employees" as a 
+inner join "public"."titles" as b on a.emp_no=b.emp_no
+where EXTRACT(YEAR from a.hire_date)>1991
+GROUP by a.emp_no
+having count(b.title)>'2'
+order by a.hire_date
 
 /*
 *  Show me all the employees that have had more than 15 salary changes that work in the department development
 *  Database: Employees
 */
 
+select a.emp_no,count(a.salary) from "public"."salaries" as a
+inner join "public"."dept_emp" as b on b.emp_no=a.emp_no 
+where b.dept_no='d005'
+GROUP by a.emp_no
+having count(a.salary)>15
+order by emp_no
+
+--11493
 
 
 /*
@@ -18,3 +31,8 @@
 *  Database: Employees
 */
 
+
+SELECT emp_no,count(dept_no) from "public"."dept_emp"
+GROUP by emp_no
+having count(dept_no)>1
+order by emp_no
