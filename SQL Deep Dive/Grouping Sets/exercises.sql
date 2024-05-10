1fc7d2edc8b22ad62d5f3f69360e46df0122f682
lucifer-430 <54584266+lucifer-430@users.noreply.github.com>
2024-05-10 15:52:35 +0530

    Update exercises.sql
    all 3 are correct ,but query format are different

--- SQL Deep Dive/Grouping Sets/exercises.sql
+++ SQL Deep Dive/Grouping Sets/exercises.sql
@@ -4,9 +4,24 @@
 *  Table: Employees
 */
 
+SELECT dept_no,count(emp_no) as "no of emp" from  "public"."dept_emp"
+group by 
+        grouping sets(
+        (),
+        (dept_no)
+        )
+order by dept_no 
+
 /*
 *  Calculate the total average salary per department and the total using grouping sets
 *  Database: Employees
 *  Table: Employees
 */
 
+select a.dept_no,avg(b.salary)from "public"."dept_emp" as a 
+inner join "public"."salaries" as b on a.emp_no=b.emp_no
+group by 
+        grouping sets(
+        (),(a.dept_no)
+        )
+order by a.dept_no
