6d2d0348c6bb522dcbfa8614ffba8f5cf78c948e
lucifer-430 <54584266+lucifer-430@users.noreply.github.com>
2024-05-27 16:28:49 +0530

    Update questions.sql
    I have to see this query

--- SQL Deep Dive/NULLIF/questions.sql
+++ SQL Deep Dive/NULLIF/questions.sql
@@ -4,6 +4,4 @@
 * Question: Show NULL when the product is not on special (0)
 */
 
-SELECT *
-FROM products
-
+SELECT prod_id,category,title,actor,price,NULLIF(special,0) as "special" from "public"."products"
