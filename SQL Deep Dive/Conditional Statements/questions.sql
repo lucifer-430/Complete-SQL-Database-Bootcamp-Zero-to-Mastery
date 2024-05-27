3b1a9309e0dcd96cd9bc160e706f3bf2e96dd2d5
lucifer-430 <54584266+lucifer-430@users.noreply.github.com>
2024-05-27 16:03:56 +0530

    Update questions.sql
    correct

--- SQL Deep Dive/Conditional Statements/questions.sql
+++ SQL Deep Dive/Conditional Statements/questions.sql
@@ -3,5 +3,13 @@
 * Table: products
 * Create a case statement that's named "price class" where if a product is over 20 dollars you show 'expensive'
 * if it's between 10 and 20 you show 'average' 
-* and of is lower than or equal to 10 you show 'cheap'.
+* and of is lower than or equal to 10 you show 'cheap'.c
 */
+SELECT prod_id,category,title,actor,price,
+case
+    when price>20 then 'expensive'
+    when (price<=20 and price>10) then 'average'
+    else 'cheap' 
+end as "price tag",
+special,common_prod_id
+from "public"."products"
